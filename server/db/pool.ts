af103ca4a0bcd81a41e699c17522b34344c4ea93
Se Joon Park <sparky9043@gmail.com>
2026-02-25 16:46:13 +0900

    build: update code to use Supabase DB

--- server/db/pool.ts
+++ server/db/pool.ts
@@ -1,12 +1,18 @@
 import { Pool } from "pg";
 import config from "../src/utils/config";
 
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: config.DATABASE_USER,
+//   database: config.DATABASE_NAME,
+//   password: config.DATABASE_PASSWORD,
+//   port: 5432,
+// });
+
+const connectionString = `postgresql://postgres.tatesxfpnjmobhrzafnz:${config.SUPABASE_DB_PASSWORD}@aws-1-ap-south-1.pooler.supabase.com:5432/postgres`;
+
 const pool = new Pool({
-  host: 'localhost',
-  user: config.DATABASE_USER,
-  database: config.DATABASE_NAME,
-  password: config.DATABASE_PASSWORD,
-  port: 5432,
+  connectionString,
 });
 
 export default pool;
