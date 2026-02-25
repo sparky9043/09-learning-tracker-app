3e8388153084008ba6c8a5006066230221462e24
Se Joon Park <sparky9043@gmail.com>
2026-02-25 15:01:35 +0900

    move Welcome navbar into its own component

--- client/src/components/DashboardPage/Welcome.tsx
+++ client/src/components/DashboardPage/Welcome.tsx
@@ -1,13 +1,50 @@
+import loginService from "@/service/loginService";
+import type { User } from "@/types/types";
+import { Link, useNavigate } from "react-router";
+import { Button } from "../ui/button";
+
 interface WelcomeProps {
-  username: string;
+  currentUser: User | null;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | null>>
 }
 
-const Welcome = (props: WelcomeProps) => {
-  if (!props.username) return null;
+const Welcome = ({ currentUser, setCurrentUser }: WelcomeProps) => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      if (currentUser) {
+        localStorage.removeItem('lastSavedUser');
+        setCurrentUser(null);
+        await loginService.logout();
+        navigate('/login');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (!currentUser || !currentUser?.username) return null;
+
 
   return (
-    <div>
-      <h1>Welcome {props.username}</h1>
+    <div className="w-25">
+      <div>
+        <h1>Welcome {currentUser.username}</h1>
+      </div>
+      <div>
+        <Button onClick={handleLogout} variant="outline" className="text-stone-800 bg-stone-200 hover:bg-stone-400 hover:border-stone-400 mb-2">log out</Button>
+      </div>
+      <div>
+        <p>Try the AI Summary!
+        </p>
+        <Link
+          to='summarize'
+          className="font-sm p-2 text-stone-800 bg-stone-200 hover:bg-stone-400 rounded-md ml-2"
+        >
+          Click Here
+        </Link>
+      </div>
     </div>
   )
 };
