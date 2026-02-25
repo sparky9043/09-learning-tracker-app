649e6d21722e031d236eb2f6f66f6fc3918035d8
Se Joon Park <sparky9043@gmail.com>
2026-02-25 15:35:22 +0900

    style: wrap div around summary component and style and center

--- client/src/components/SummaryPage/Summary.tsx
+++ client/src/components/SummaryPage/Summary.tsx
@@ -9,45 +9,47 @@ const Summary = () =>  {
   const { openaiResponse, setOpenaiResponse } = useOpenaiResponseContext();
 
   return (
-    <div className="flex flex-col w-1/2 border-2">
-      <h2>
-        Summary
-      </h2>
-      <Link to='/dashboard'>Back</Link>
-      <PromptForm
-        openaiResponse={openaiResponse}
-        setOpenaiResponse={setOpenaiResponse}
-      />
-      <div>
+    <div className="flex justify-center p-4">
+      <div className="flex flex-col w-1/2 p-4 border-2 border-stone-200 shadow-xl rounded-md">
+        <h2>
+          Summary
+        </h2>
+        <Link to='/dashboard'>Back</Link>
+        <PromptForm
+          openaiResponse={openaiResponse}
+          setOpenaiResponse={setOpenaiResponse}
+        />
+        <div>
+          {
+            openaiResponse !== null
+              ? <div>
+                  <p>
+                    <strong>Topic</strong>: {openaiResponse.topic}
+                  </p>
+                  <p>
+                    <strong>Summary</strong>: {openaiResponse.note}
+                  </p>
+                </div>
+              : null
+          }
+        </div>
         {
-          openaiResponse !== null
-            ? <div>
+          openaiResponse
+            ? 
+              <div>
                 <p>
-                  <strong>Topic</strong>: {openaiResponse.topic}
+                  are you satisfied with this summary? If not, click the retry button
                 </p>
                 <p>
-                  <strong>Summary</strong>: {openaiResponse.note}
+                  If you are, click below to open forms
                 </p>
+                <Togglelable text="use ai response">
+                  <AddAiSummaryForm />
+                </Togglelable>
               </div>
             : null
         }
       </div>
-      {
-        openaiResponse
-          ? 
-            <div>
-              <p>
-                are you satisfied with this summary? If not, click the retry button
-              </p>
-              <p>
-                If you are, click below to open forms
-              </p>
-              <Togglelable text="use ai response">
-                <AddAiSummaryForm />
-              </Togglelable>
-            </div>
-          : null
-      }
     </div>
   )
 };
