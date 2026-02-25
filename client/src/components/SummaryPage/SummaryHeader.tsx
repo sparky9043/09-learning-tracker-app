3921c3508c6b970e4f795d569393fd6bf1e3e263
Se Joon Park <sparky9043@gmail.com>
2026-02-25 15:51:20 +0900

    chore: change Link to Button and set ai response to null when moving to previous  page

--- client/src/components/SummaryPage/SummaryHeader.tsx
+++ client/src/components/SummaryPage/SummaryHeader.tsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router";
+import useOpenaiResponseContext from "@/hooks/useOpenaiResponseContext";
+import { useNavigate } from "react-router";
+import { Button } from "../ui/button";
 
 const SummaryHeader = () => {
+  const { setOpenaiResponse } = useOpenaiResponseContext();
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    setOpenaiResponse(null);
+    navigate(-1);
+  };
+
   return (
     <div className="flex justify-between">
       <h2>
         Summary
       </h2>
-      <Link to='/dashboard'>
+      <Button onClick={handleBack}>
         &larr; Back 
-      </Link>
+      </Button>
     </div>
   )
 };
