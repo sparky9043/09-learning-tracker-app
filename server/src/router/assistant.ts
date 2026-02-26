66fea568848745501e5eb7974f347ad48b557309
Se Joon Park <sparky9043@gmail.com>
2026-02-26 09:30:07 +0900

    chore: add AI assistant route file

---
+++ server/src/router/assistant.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response, Router } from "express";
+
+const assistant = Router();
+
+assistant.post('/', (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send('connected');
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default assistant;
