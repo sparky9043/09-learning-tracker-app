fe22545953e0ef2ab8f53b0ad316ccd6a2537bdb
Se Joon Park <sparky9043@gmail.com>
2026-02-26 09:51:31 +0900

    chore: make POST route accept study question request

--- server/src/router/assistant.ts
+++ server/src/router/assistant.ts
@@ -1,9 +1,19 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { AIGenerateStudyQuestionRequest } from "../types/types";
 
 const assistant = Router();
 
 assistant.get('/', (_req: Request, res: Response, next: NextFunction) => {
   try {
+    res.send('connected');
+  } catch (error) {
+    next(error);
+  }
+});
+
+assistant.post('/', (req: Request<unknown, unknown, AIGenerateStudyQuestionRequest>, res: Response, next: NextFunction) => {
+  try {
+    console.log(req.body);
     res.send('connected');
   } catch (error) {
     next(error);
