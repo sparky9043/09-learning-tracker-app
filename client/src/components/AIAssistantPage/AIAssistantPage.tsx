1ea3f44526cd1855ef14ecb1ecf87d853dc4303f
Se Joon Park <sparky9043@gmail.com>
2026-02-27 18:09:39 +0900

    render entries by user

--- client/src/components/AIAssistantPage/AIAssistantPage.tsx
+++ client/src/components/AIAssistantPage/AIAssistantPage.tsx
@@ -1,7 +1,54 @@
+import entriesService from "@/service/entriesService";
+import type { SavedLearningEntry } from "@/types/types";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 const AIAssistantPage = () => {
+  const [selected, notSelected] = useState();
+
+  const entryByUserQuery = useQuery<SavedLearningEntry[]>({
+    queryKey: ['entriesByUser'],
+    queryFn: entriesService.getAllEntriesByUser,
+  });
+
+  if (entryByUserQuery?.isLoading) {
+    return <div>
+      Loading Entries By User...
+    </div>
+  }
+
+  if (!entryByUserQuery.data) {
+    return <div>
+      an unexpected error occurred...
+    </div>
+  }
+
+  if (entryByUserQuery.isError) {
+    return <div>
+      hello
+    </div>
+  }
+
+  console.log(entryByUserQuery.data);
+  if (!entryByUserQuery.data) {
+    return (
+      <div>
+        Data not loaded
+      </div>
+    )
+  }
+  
   return (
     <div>
       AI Assistant Page
+      {entryByUserQuery.data.map(e => <li key={e.id}>
+        <p>
+          Topic: {e.topic}
+        </p>
+        <p>
+          Summary: {e.note}
+        </p>
+      </li>)}
     </div>
   )
 };
