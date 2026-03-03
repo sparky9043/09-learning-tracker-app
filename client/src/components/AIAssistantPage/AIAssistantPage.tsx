254537f394e0980423222e69612431f9d5e02fb0
Se Joon Park <sparky9043@gmail.com>
2026-03-03 15:46:18 +0900

    fix: update code to take list of all checked boxes

--- client/src/components/AIAssistantPage/AIAssistantPage.tsx
+++ client/src/components/AIAssistantPage/AIAssistantPage.tsx
@@ -1,12 +1,12 @@
 import entriesService from "@/service/entriesService";
-import { type AIUserEntryInput, type SavedLearningEntry } from "@/types/types";
+import { type SavedLearningEntry } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 import { Button } from "../ui/button";
 import EntryItem from "./EntryItem";
-import { useState } from "react";
+// import { useState } from "react";
 
 const AIAssistantPage = () => {
-  const [selectedEntries, setSelectedEntries] = useState<AIUserEntryInput[]>([]);
+  // const [selectedEntries, setSelectedEntries] = useState<AIUserEntryInput[]>([]);
 
   const entryByUserQuery = useQuery<SavedLearningEntry[]>({
     queryKey: ['entriesByUser'],
@@ -41,35 +41,42 @@ const AIAssistantPage = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    // const entriesToSubmit = {
+    //   concepts: selectedEntries
+    const data = Array.from((event.target as HTMLFormElement).entry) as HTMLInputElement[];
 
-    const entriesToSubmit = {
-      concepts: selectedEntries
-    }
+    const selectedList = data.filter(entry => {
+      return entry.checked;
+    });
 
-    console.log(entriesToSubmit);
+    console.log(selectedList);
 
+    // for (const input of data) {
+    //   const inputEntry = input as HTMLInputElement;
+    //   inputEntry.checked = false;
+    // }
   };
 
-  const handleClick = (entryId: number) => {
-    const entry = entryByUserQuery.data.find(entry => Number(entry.id) === Number(entryId));
+  // const handleClick = (entryId: number) => {
+  //   const entry = entryByUserQuery.data.find(entry => Number(entry.id) === Number(entryId));
 
-    if (!entry || !entry.note || !entry.topic) {
-      throw new Error('the note you selected is not found');
-    }
+  //   if (!entry || !entry.note || !entry.topic) {
+  //     throw new Error('the note you selected is not found');
+  //   }
 
-    const isEntrySelected = selectedEntries.map(entry => entry.note).includes(entry?.note)
+  //   const isEntrySelected = selectedEntries.map(entry => entry.note).includes(entry?.note)
 
-    if (entry?.note && !isEntrySelected) {
-      setSelectedEntries(prev => [...prev, { note: entry?.note, topic: entry?.topic }])
-    }
+  //   if (entry?.note && !isEntrySelected) {
+  //     setSelectedEntries(prev => [...prev, { id: entry?.id ,note: entry?.note, topic: entry?.topic }])
+  //   }
 
-  };
+  // };
   
   return (
     <div>
       AI Assistant Page
-      {entryByUserQuery.data.map(entry => <EntryItem key={entry.id} entry={entry} onClick={handleClick} />)}
       <form onSubmit={handleSubmit}>
+        {entryByUserQuery.data.map(entry => <EntryItem key={entry.id} entry={entry} />)}
         <Button>Submit</Button>
       </form>
     </div>
--- client/src/components/AIAssistantPage/EntryItem.tsx
+++ client/src/components/AIAssistantPage/EntryItem.tsx
@@ -1,20 +1,29 @@
 import type { SavedLearningEntry } from "@/types/types";
-import { Button } from "../ui/button";
+import { useState } from "react";
 
 interface EntryItemProps {
   entry: SavedLearningEntry;
-  onClick: (entryId: number) => void;
+  // onClick: (entryId: number) => void;
 }
 
 const EntryItem = (props: EntryItemProps) => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
   return (
-    <div>
+    <div className="border-2 p-4 m-4">
+      <input
+        id={String(props.entry.id)}
+        type="checkbox"
+        checked={isSelected}
+        name="entry"
+        onChange={(e) => setIsSelected(e.target.checked)}
+      />
       <ul>
         <li>{props.entry.topic}</li>
         <li>{props.entry.note}</li>
-        <li>
+        {/* <li>
           <Button onClick={() => props.onClick(props.entry.id)}>Add </Button>
-        </li>
+        </li> */}
       </ul>
     </div>
   )
