dd85076ab4b1aafdd5fc2d3b49beced4267fc566
Se Joon Park <sparky9043@gmail.com>
2026-03-05 14:38:38 +0900

    feat: add instructions on how  to use the AI Assistant

--- client/src/components/AIAssistantPage/AIAssistantPage.tsx
+++ client/src/components/AIAssistantPage/AIAssistantPage.tsx
@@ -91,6 +91,17 @@ const AIAssistantPage = () => {
     <div className="grid grid-cols-2">
       <form onSubmit={handleSubmit}>
         <h2>AI Assistant Page</h2>
+        <ol>
+          <li>
+            1. Select learning topics that you want to learn more about using checkbox
+          </li>
+          <li>
+            2. Press Submit and wait for the AI to generate questions based on the topics you sent (3 questions per topic)
+          </li>
+          <li>
+            3. Use the AI Generated Questions as guides to learn more about the topic
+          </li>
+        </ol>
         {entryByUserQuery.data.map(entry =>
           <EntryItem
             key={entry.id}
