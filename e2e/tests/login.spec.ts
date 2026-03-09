87b898a3b6e89fb7b819ec48f2d532804269ffa2
Se Joon Park <sparky9043@gmail.com>
2026-03-09 12:08:03 +0900

    test: make each test go to main title page before each test

--- e2e/tests/login.spec.ts
+++ e2e/tests/login.spec.ts
@@ -1,40 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-const home_url = 'http://localhost:5173'
+const home_url = 'http://localhost:5173';
 
-test('has title', async ({ page }) => {
-  await page.goto(home_url);
+test.describe('Get LOGIN Page', () => {
+  test.beforeEach(async ({ page }) =>  {
+    await page.goto(home_url);
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/client/);
-});
-
-test('login link displays login page', async ({ page }) => {
-  await page.goto(home_url);
-
-  await page.getByRole('link', { name: 'Login' }).click();
-
-  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
-});
-
-// Login using username and password
-test('login with username and password', async ({ page }) => {
-  await page.goto(`${home_url}/login`);
-
-  await page.getByLabel('Username').fill('default');
-  await page.getByLabel('Password').fill('password123');
+  test('has title', async ({ page }) => {
+    // await page.goto(home_url);
   
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  await expect(page.getByRole('heading', { name: /Welcome/ })).toBeVisible();
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/client/);
+  });
+  
+  test('login link displays login page', async ({ page }) => {
+    // await page.goto(home_url);
+  
+    await page.getByRole('link', { name: 'Login' }).click();
+  
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+  });
+  
+  // Login using username and password
+  test('login with username and password', async ({ page }) => {
+    await page.getByRole('link', { name: 'Login' }).click();
+  
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    
+    await page.goto(`${home_url}/login`);
+  
+    await page.getByLabel('Username').fill('default');
+    await page.getByLabel('Password').fill('password123');
+  
+    await page.getByRole('button', { name: 'Login' }).click();
+  
+    await expect(page.getByRole('heading', { name: /Welcome/ })).toBeVisible();
+  });
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
