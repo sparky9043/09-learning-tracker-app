4609a2eb74afa4a7628a65903e29a4ae610834a4
Se Joon Park <sparky9043@gmail.com>
2026-03-12 16:10:53 +0900

    feat: add heading to each LearningEntryItem

--- client/src/components/DashboardPage/LearningEntryItem.tsx
+++ client/src/components/DashboardPage/LearningEntryItem.tsx
@@ -33,7 +33,11 @@ const LearningEntryItem = ({ entry }: LearningEntryItemProps) => {
     <li key={entry.id} className="p-2">
       <Card className="h-full text-center text-2xl shadow-lg min-w-100 max-w-150">
         <CardHeader>
-          <CardHeader className="margin-0 padding-0 text-shadow-md text-shadow-stone-300">{entry.topic}</CardHeader>
+          <CardHeader className="margin-0 padding-0 text-shadow-md text-shadow-stone-300">
+            <h2>
+              {entry.topic}
+            </h2>
+          </CardHeader>
           <CardDescription>
             <p>
               time spent: {entry.minutes_spent} minutes
