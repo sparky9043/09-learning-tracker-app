a4bd1e826eb9f1d16bc6e0965b099a511d356d31
Se Joon Park <sparky9043@gmail.com>
2026-03-19 07:46:26 +0900

    feat: sort extracted dates and convert to month, year and time spent

--- client/src/components/DashboardPage/Dashboard.tsx
+++ client/src/components/DashboardPage/Dashboard.tsx
@@ -4,6 +4,7 @@ import entriesService from "@/service/entriesService";
 import type { SavedLearningEntry } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
 import LoadingWheel from "../misc/LoadingWheel";
+import { compareAsc, format } from "date-fns";
 // import Welcome from "./Welcome";
 // import Togglelable from "../misc/Toggleable";
 
@@ -32,9 +33,26 @@ const Dashboard = () => {
     )
   }
 
+  // Extract timestamp and time spent from entries
   const extractDates = entryByUser.data.map(({ created_at, minutes_spent }) => ({ created_at, minutes_spent }));
 
-  console.log(extractDates)
+  const extractedDatesSorted = [...extractDates].sort((a, b) => compareAsc(a.created_at, b.created_at));
+
+
+  // Convert each entry into month, year, and time spent. Convert month and year to numbers
+  const convertToMonth = extractedDatesSorted.map(entry => ({
+    month: Number(format(entry.created_at, 'MM')),
+    year: Number(format(entry.created_at, 'yyyy')),
+    minutes_spent: entry.minutes_spent,
+  }));
+
+  // // Sort each entry By Year first and then Month
+  // const sortByYearAndMonth = [...convertToMonth]
+  //   .sort((a, b) => compareAsc(new Date(a.year, a.month, a.day), new Date(b.year, b.month, b.day)))
+    // .sort((a, b) => a.month - b.month)
+    // .sort((a, b) => a.day - b.year);
+
+  console.log(convertToMonth);
 
   return (
     <div className="flex flex-col min-h-dvh w-full">
