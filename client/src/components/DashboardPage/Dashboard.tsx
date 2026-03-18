e99e0e290ef923c790ae5e8981041717a555fa60
Se Joon Park <sparky9043@gmail.com>
2026-03-18 10:33:49 +0900

    feat: get entries by user and show loading wheel while fetching

--- client/src/components/DashboardPage/Dashboard.tsx
+++ client/src/components/DashboardPage/Dashboard.tsx
@@ -1,10 +1,18 @@
 // import LearningEntriesList from "./LearningEntriesList";
 import { useCurrentUserContext } from "@/hooks/useCurrentUserContext";
+import entriesService from "@/service/entriesService";
+import type { SavedLearningEntry } from "@/types/types";
+import { useQuery } from "@tanstack/react-query";
+import LoadingWheel from "../misc/LoadingWheel";
 // import Welcome from "./Welcome";
 // import Togglelable from "../misc/Toggleable";
 
 const Dashboard = () => {
   const { currentUser } = useCurrentUserContext();
+  const entryByUser = useQuery<SavedLearningEntry[]>({
+    queryKey: ['entriesByUser'],
+    queryFn: entriesService.getAllEntriesByUser,
+  });
 
   if (!currentUser) {
     return (
@@ -14,10 +22,18 @@ const Dashboard = () => {
     )
   }
 
+  // const isDataLoaded = Array.isArray(entryByUser.data);
+
+  console.log(entryByUser.data);
+
   return (
-    <div className="text-center flex min-h-dvh w-full">
+    <div className="flex flex-col min-h-dvh w-full">
       {/* <LearningEntriesList /> */}
       <p>Main Menu</p>
+      {entryByUser.isLoading && <div>
+          <LoadingWheel />
+        </div>
+      }
     </div>
   )
 }
