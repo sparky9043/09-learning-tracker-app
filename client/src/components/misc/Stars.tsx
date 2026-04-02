44c15ccea4db93a3c5a9aaaabfbebb0b4686225f
Se Joon Park <sparky9043@gmail.com>
2026-04-02 09:20:29 +0900

    feat: create stars based on difficulty and max stars

--- client/src/components/misc/Stars.tsx
+++ client/src/components/misc/Stars.tsx
@@ -3,19 +3,24 @@ interface StarsProps {
 }
 
 const Stars = ({ difficulty }: StarsProps) => {
+  const MAX_STARS = 5;
+
+  const difficultyArray = () => Array
+    .from({ length: MAX_STARS },
+        (_, i) => <span
+          key={i}
+          className="material-symbols-outlined text-sm!"
+          style={{ fontVariationSettings: `'FILL' ${i < difficulty ? 1 : 0}` }}>
+            star
+        </span>
+    )
+
   return (
-            <div className="flex items-center gap-1"><span className="text-tertiary font-bold mr-1">{difficulty}/5</span>
-              <div className="flex text-tertiary"><span className="material-symbols-outlined text-sm!"
-                 style={{ fontVariationSettings: "'FILL' 1" }}>star</span><span
-                  className="material-symbols-outlined text-sm!"
-                  style={{ fontVariationSettings: "'FILL' 1" }}>star</span><span
-                    className="material-symbols-outlined text-sm!"
-                    style={{ fontVariationSettings: "'FILL' 1" }}>star</span><span
-                      className="material-symbols-outlined text-sm!"
-                      style={{ fontVariationSettings: "'FILL' 1" }}>star</span><span
-                        className="material-symbols-outlined text-sm!" style={{ fontVariationSettings: "'FILL' 1" }}>star</span>
-              </div>
-            </div>
+    <div className="flex items-center gap-1"><span className="text-tertiary font-bold mr-1">{difficulty}/5</span>
+      <div className="flex text-tertiary">
+        {difficultyArray()}
+      </div>
+    </div>
   )
 };
 
