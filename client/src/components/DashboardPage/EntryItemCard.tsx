4655c24183aa6ad12094b5e167585db7507fb62b
Se Joon Park <sparky9043@gmail.com>
2026-04-02 11:05:17 +0900

    fix: show delete button upon hovering each entry item

--- client/src/components/DashboardPage/EntryItemCard.tsx
+++ client/src/components/DashboardPage/EntryItemCard.tsx
@@ -1,56 +1,46 @@
 import type { SavedLearningEntry } from "@/types/types";
 import { format } from "date-fns";
 import Stars from "../misc/Stars";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import entriesService from "@/service/entriesService";
 
 interface EntryItemCardItemProps {
   entry: SavedLearningEntry;
 }
 
 const EntryItemCard = ({ entry }: EntryItemCardItemProps) => {
+  const client = useQueryClient();
 
-  // return (
-  //   <div
-  //     className="group bg-surface-container-low p-6 rounded-xl hover:bg-surface-container transition-all duration-300 refined-border">
-  //     <div className="flex justify-between items-start mb-4">
-  //       <div className="flex justify-between items-start mb-6">
-  //         <div
-  //           className="w-10 h-10 rounded-lg bg-primary/5 flex items-center justify-center text-primary group-hover:bg-primary/10 transition-colors">
-  //           <span className="material-symbols-outlined text-xl" data-icon="code">code</span>
-  //         </div>
-  //       </div>
-  //       <div className="flex gap-4">
-  //         <div>
-  //           <h3 className="font-headline font-bold text-xl text-on-surface">{entry.topic}</h3>
-  //           <p className="text-sm text-on-surface-variant font-body">{entry.note}</p>
-  //         </div>
-  //       </div>
-  //       {/* <span
-  //         className="material-symbols-outlined text-outline group-hover:text-primary transition-colors">more_vert</span> */}
-  //     </div>
-  //     <div className="space-y-2">
-  //       <div className="flex justify-between text-xs font-label text-on-surface-variant">
-  //         <span>Date: {format(entry.created_at, 'MMM do yyyy')}</span>
-  //         <span className="uppercase">Difficulty</span>
-  //       </div>
-  //       <div className="space-y-2">
-  //         <div className="flex justify-between items-center text-xs font-label text-on-surface-variant">
-  //           <span>Time Spent: {entry.minutes_spent} min</span>
-  //           <Stars difficulty={entry.difficulty} />
-  //         </div>
-  //       </div>
-  //       {/* <div className="h-1 w-full bg-surface-container-highest rounded-full overflow-hidden">
-  //         <div className="h-full bg-tertiary" style={{ width: `${(entry.difficulty / 5) * 100}%` }}></div>
-  //       </div> */}
-  //     </div>
-  //   </div>
-  // )
+  const deleteEntryMutation = useMutation({
+    mutationKey: ['entriesByUser'],
+    mutationFn: entriesService.deleteEntryByUser,
+    onSuccess: () => {
+      client.invalidateQueries({
+        queryKey: ['entriesByUser'],
+      })
+    }
+  });
+
+  const handleDelete = (entryId: number) => {
+    console.log(entryId);
+    deleteEntryMutation.mutate(entryId);
+  }
+
   return (
     <div
       className="group bg-surface-container-low p-6 rounded-xl hover:bg-surface-container transition-all duration-300 refined-border">
-      <div className="flex justify-between items-start mb-6">
+      <div className="flex justify-between items-start mb-6 relative">
         <div
           className="w-10 h-10 rounded-lg bg-primary/5 flex items-center justify-center text-primary group-hover:bg-primary/10 transition-colors">
           <span className="material-symbols-outlined text-xl" data-icon="code">code</span>
+        </div>
+        <div className="mb-6 mt-6 absolute right-0 hidden group-hover:block">
+          <button
+            className="w-10 h-10 rounded-lg flex items-center justify-center text-primary group-hover:text-error-container transition-colors hover:cursor-pointer"
+            onClick={() => handleDelete(entry.id)}  
+          >
+            <span className="material-symbols-outlined text-xl" data-icon="code">delete</span>
+          </button>
         </div>
         <Stars difficulty={entry.difficulty} />
       </div>
@@ -67,6 +57,7 @@ const EntryItemCard = ({ entry }: EntryItemCardItemProps) => {
           {entry.minutes_spent}m
         </div>
       </div>
+      {/* <button onClick={() => handleDelete(entry.id)}>delete</button> */}
     </div>
   )
 };
