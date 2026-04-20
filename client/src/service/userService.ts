0a499d6c4739c57b7b8d0c90dd3cff8e8647843e
Se Joon Park <sparky9043@gmail.com>
2026-04-20 17:15:57 +0900

    feat: make createUser function

--- client/src/service/userService.ts
+++ client/src/service/userService.ts
@@ -1,10 +1,16 @@
-import axios from "axios"
+import axios from "axios";
+import type { UserCredential } from "@/types/types";
 
-const baseUrl = '/api/users'
+const baseUrl = '/api/users';
 
 const getUsers = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 }
 
-export default { getUsers };
+const createUser = async (userCredential: UserCredential) => {
+  const response = await axios.post(baseUrl, userCredential);
+  return response.data;
+};
+
+export default { getUsers, createUser };
